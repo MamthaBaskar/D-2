4f43944764a7652fd5aa05f6f43a4fb182bf6534
Mamtha Baskar <mamtha1655@gmail.com>
2026-02-05 17:56:21 +0530

    Added new module

--- lib.java
+++ lib.java
@@ -1,5 +1,6 @@
 public class LibraryManagementSystems{
     public static void main(String[] argd){
         System.out.println("Library Management Systems")
+        System.out.println("Student Registration Module Added");
     }
 }
